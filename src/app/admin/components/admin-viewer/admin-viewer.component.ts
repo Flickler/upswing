50f6205d924a995393633f5939b6cb0217b4f65c
Manoel Junior <manoelj1240@gmail.com>
2024-01-27 19:17:49 -0300

    feat: imported icons

---
+++ src/app/admin/components/admin-viewer/admin-viewer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-viewer',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-viewer.component.html',
+  styleUrl: './admin-viewer.component.scss',
+})
+export class AdminViewerComponent {
+  admin = [];
+}
--- src/app/icons/lucide-icons.ts
+++ src/app/icons/lucide-icons.ts
@@ -1,4 +1,82 @@
 export const icons = {
+  alertCircle: {
+    lines: [
+      { x1: 12, x2: 12, y1: 8, y2: 12 },
+      { x1: 12, x2: 12.01, y1: 16, y2: 16 },
+    ],
+    circles: [{ cx: 12, cy: 12, r: 10 }],
+  },
+
+  alertOctagon: {
+    lines: [
+      { x1: 12, x2: 12, y1: 8, y2: 12 },
+      { x1: 12, x2: 12.01, y1: 16, y2: 16 },
+    ],
+    polygons: [
+      '7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2',
+    ],
+  },
+
+  arrowUpRight: {
+    paths: ['M7 7h10v10', 'M7 17 17 7'],
+  },
+
+  atSign: {
+    circles: [{ cx: 12, cy: 12, r: 4 }],
+    paths: ['M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-4 8'],
+  },
+
+  bell: {
+    paths: [
+      'M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9',
+      'M10.3 21a1.94 1.94 0 0 0 3.4 0',
+    ],
+  },
+
+  bot: {
+    paths: ['M12 8V4H8', 'M2 14h2', 'M20 14h2', 'M15 13v2', 'M9 13v2'],
+
+    rects: [{ width: 16, height: 12, x: 4, y: 8, rx: 2 }],
+  },
+
+  bookOpen: {
+    paths: [
+      'M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z',
+      'M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z',
+    ],
+  },
+
+  briefcase: {
+    rects: [{ width: 20, height: 14, x: 2, y: 7, rx: 2, ry: 2 }],
+    paths: ['M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16'],
+  },
+
+  building: {
+    rects: [{ width: 16, height: 20, x: 4, y: 2, rx: 2, ry: 2 }],
+    paths: [
+      'M9 22v-4h6v4',
+      'M8 6h.01',
+      'M16 6h.01',
+      'M12 6h.01',
+      'M12 10h.01',
+      'M12 14h.01',
+      'M16 10h.01',
+      'M16 14h.01',
+      'M8 10h.01',
+      'M8 14h.01',
+    ],
+  },
+
+  camera: {
+    paths: [
+      'M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z',
+    ],
+    circles: [{ cx: 12, cy: 13, r: 3 }],
+  },
+  checkCircle2: {
+    paths: ['m9 12 2 2 4-4'],
+    circles: [{ cx: 12, cy: 12, r: 10 }],
+  },
   circleUser: {
     paths: ['M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662'],
     circles: [
@@ -6,10 +84,43 @@ export const icons = {
       { cx: 12, cy: 10, r: 3 },
     ],
   },
+  chevronDown: {
+    paths: ['m6 9 6 6 6-6'],
+  },
+
+  chevronLeft: {
+    paths: ['m15 18-6-6 6-6'],
+  },
+
+  chevronRight: {
+    paths: ['m9 18 6-6-6-6'],
+  },
+
+  chevronUp: {
+    paths: ['m18 15-6-6-6 6'],
+  },
+
+  chevronLeftSquare: {
+    paths: ['m14 16-4-4 4-4'],
+    rects: [{ width: 18, height: 18, x: 3, y: 3, rx: 2 }],
+  },
+
+  circleUserRound: {
+    paths: ['M18 20a6 6 0 0 0-12 0'],
+    circles: [
+      { cx: 12, cy: 10, r: 4 },
+      { cx: 12, cy: 12, r: 10 },
+    ],
+  },
+
   eye: {
-    paths: ['M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z'],
+    paths: [
+      'M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z',
+      'M12 5v1m6 6 12 12',
+    ],
     circles: [{ cx: 12, cy: 12, r: 3 }],
   },
+
   eyeOff: {
     paths: [
       'M9.88 9.88a3 3 0 1 0 4.24 4.24',
@@ -18,16 +129,158 @@ export const icons = {
     ],
     lines: [{ x1: 2, x2: 22, y1: 2, y2: 22 }],
   },
-  bot: {
-    paths: ['M12 8V4H8', 'M2 14h2', 'M20 14h2', 'M15 13v2', 'M9 13v2'],
-    rects: [{ width: 16, height: 12, x: 4, y: 8, rx: 2 }],
+
+  fileText: {
+    paths: [
+      'M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z',
+      'M14 2v4a2 2 0 0 0 2 2h4',
+      'M10 9H8',
+      'M16 13H8',
+      'M16 17H8',
+    ],
   },
-  briefcase: {
-    rects: [{ width: 20, height: 14, x: 2, y: 7, rx: 2, ry: 2 }],
-    paths: ['M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16'],
+
+  graduationCap: {
+    paths: ['M22 10v6M2 10l10-5 10 5-10 5z', 'M6 12v5c3 3 9 3 12 0v-5'],
   },
+
+  helpCircle: {
+    circles: [{ cx: 12, cy: 12, r: 10 }],
+    paths: ['M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3', 'M12 17h.01'],
+  },
+
+  info: {
+    circles: [{ cx: 12, cy: 12, r: 10 }],
+    paths: ['M12 16v-4', 'M12 8h.01'],
+  },
+
+  link2: {
+    lines: [{ x1: 8, x2: 16, y1: 12, y2: 12 }],
+    paths: ['M9 17H7A5 5 0 0 1 7 7h2', 'M15 7h2a5 5 0 1 1 0 10h-2'],
+  },
+
+  logOut: {
+    lines: [{ x1: 21, x2: 9, y1: 12, y2: 12 }],
+    paths: ['M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4'],
+    polylines: ['16 17 21 12 16 7'],
+  },
+
+  mail: {
+    paths: ['m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7'],
+    rects: [{ width: 20, height: 16, x: 2, y: 4, rx: 2 }],
+  },
+
+  mapPin: {
+    circles: [{ cx: 12, cy: 10, r: 3 }],
+    paths: ['M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z'],
+  },
+
+  menu: {
+    lines: [
+      { x1: 4, x2: 20, y1: 12, y2: 12 },
+      { x1: 4, x2: 20, y1: 6, y2: 6 },
+      { x1: 4, x2: 20, y1: 18, y2: 18 },
+    ],
+  },
+
+  moreVertical: {
+    circles: [
+      { cx: 12, cy: 12, r: 1 },
+      { cx: 12, cy: 5, r: 1 },
+      { cx: 12, cy: 19, r: 1 },
+    ],
+  },
+
+  mouse: {
+    paths: ['M12 6v4'],
+    rects: [{ width: 14, height: 20, x: 5, y: 2, rx: 7 }],
+  },
+
+  phone: {
+    paths: [
+      'M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z',
+    ],
+  },
+
   plus: {
     paths: ['M5 12h14', 'M12 5v14'],
+  },
+
+  search: {
+    circles: [{ cx: 11, cy: 11, r: 8 }],
+    paths: ['m21 21-4.3-4.3'],
+  },
+
+  squarePen: {
+    paths: [
+      'M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7',
+      'M18.375 2.625a2.121 2.121 0 1 1 3 3L12 15l-4 1 1-4Z',
+    ],
+  },
+
+  star: {
+    polygons: [
+      '12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2',
+    ],
+  },
+
+  shield: {
+    paths: ['M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10'],
+  },
+
+  thumbsUp: {
+    paths: [
+      'M7 10v12',
+      'M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z',
+    ],
+  },
+
+  trash2: {
+    lines: [
+      { x1: 10, x2: 10, y1: 11, y2: 17 },
+      { x1: 14, x2: 14, y1: 11, y2: 17 },
+    ],
+    paths: [
+      'M3 6h18',
+      'M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6',
+      'M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2',
+    ],
+  },
+
+  userRound: {
+    circles: [{ cx: 12, cy: 8, r: 5 }],
+
+    paths: ['M20 21a8 8 0 0 0-16 0'],
+  },
+
+  squareUserRound: {
+    circles: [{ cx: 12, cy: 11, r: 4 }],
+    paths: ['M18 21a6 6 0 0 0-12 0'],
+    rects: [{ width: 18, height: 18, x: 3, y: 3, rx: 2 }],
+  },
+
+  x: {
+    paths: ['M18 6 6 18', 'm6 6 12 12'],
+  },
+
+  facebook: {
+    paths: [
+      'M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z',
+    ],
+  },
+
+  instagram: {
+    lines: [{ x1: 17.5, x2: 17.51, y1: 6.5, y2: 6.5 }],
+    paths: ['M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z'],
+    rects: [{ width: 20, height: 20, x: 2, y: 2, rx: 5, ry: 5 }],
+  },
+
+  linkedin: {
+    circles: [{ cx: 4, cy: 4, r: 2 }],
+    paths: [
+      'M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z',
+    ],
+    rects: [{ width: 4, height: 12, x: 2, y: 9 }],
   },
 };
 
