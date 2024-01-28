ce98a275f960bb0506fb91ac2a0ca802d8c11830
Manoel Junior <manoelj1240@gmail.com>
2024-01-28 00:01:29 -0300

    feat: created admin viewer

--- src/app/admin/components/admin-viewer/admin-viewer.component.ts
+++ src/app/admin/components/admin-viewer/admin-viewer.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { ListService } from '@@Services/list.service';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { map } from 'rxjs';
+
+import { LucideIcons } from '@@Icons/lucide-icons.component';
+import { Pagination } from '@@Types/Pagination';
+import { Admins } from '@@Types/Admin';
 
 @Component({
   selector: 'app-admin-viewer',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, LucideIcons],
   templateUrl: './admin-viewer.component.html',
   styleUrl: './admin-viewer.component.scss',
 })
 export class AdminViewerComponent {
-  admin = [];
+  listService = inject(ListService);
+  admins$ = this.listService
+    .listAdmins(0)
+    .pipe(map((pagination: Pagination<Admins>) => pagination.content));
 }
