2fc8f50cd7e4ca7d57bf132f72c26832e616d172
Manoel Junior <manoelj1240@gmail.com>
2024-01-28 00:00:36 -0300

    feat: created listService

---
+++ src/app/services/list.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@@Environments/environment';
+import { Pagination } from '@@Types/Pagination';
+import { Admin, Admins } from '@@Types/Admin';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ListService {
+  private readonly path = environment.apiUrl + '/list';
+  private http = inject(HttpClient);
+
+  listAdmins(page: number) {
+    return this.http.get<Pagination<Admins>>(this.path + '/admin?page=' + page);
+  }
+
+  listAdminById(id: string) {
+    return this.http.get<Admin>(this.path + '/admin/' + id);
+  }
+}
