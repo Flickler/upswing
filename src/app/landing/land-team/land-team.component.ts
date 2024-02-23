49193d6ee77c5a9f64f4f8b681aa4de86d4d28f2
Manoel Junior <manoelj1240@gmail.com>
2024-02-23 06:58:00 -0300

    feat: add popup sound, user dashboard image and fix some landing overflow layout

---
+++ src/app/landing/land-team/land-team.component.ts
@@ -0,0 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'land-team',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './land-team.component.html',
+  styleUrl: './land-team.component.scss',
+})
+export class LandTeamComponent {}
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,11 +3,17 @@ import { Component } from '@angular/core';
 import { LandHeaderComponent } from './land-header/land-header.component';
 import { LucideIcons } from '@@Icons/lucide-icons.component';
 import { LandFooterComponent } from './land-footer/land-footer.component';
+import { LandTeamComponent } from './land-team/land-team.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [LandHeaderComponent, LandFooterComponent, LucideIcons],
+  imports: [
+    LandHeaderComponent,
+    LandFooterComponent,
+    LandTeamComponent,
+    LucideIcons,
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
